9802fe552386399ce4a3b9b34d9d5311dbee77c7
RawCooked <123646029+RawCooked@users.noreply.github.com>
2023-12-08 20:05:44 +0100

    added required files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pcg66/local-art',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eff03cbf8cbaa649227e5d31b3553518a9ce5916',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pcg66/local-art' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eff03cbf8cbaa649227e5d31b3553518a9ce5916',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
